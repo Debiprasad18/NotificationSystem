033a4c04a57b087795464564838172c279f3f8f1
Debiprasad <debiprasad18apr@gmail.com>
2020-05-15 15:27:53 +0530

    Changes

--- src/main/java/com/nineleaps/notification/service/Consumer.java
+++ src/main/java/com/nineleaps/notification/service/Consumer.java
@@ -1,11 +1,13 @@
 package com.nineleaps.notification.service;
 
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
 
+@Service
 public class Consumer {
 
 	@KafkaListener(topics = "users", groupId = "group_id")
 	public void consume(String message) {
-		System.out.println(String.format("$$ -> Consumed Message -> %s", message));
+		System.out.println("Consumed Message --->"+ message);
 	}
 }
